9152f5906f15d1d53d11e3863747a6f960f9a58e
hermany13 <hermanyvieiratj@gmail.com>
2020-04-12 11:32:26 -0300

    Structure changes.

---
+++ pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Styled Components
+import { ContainerView, Text } from "./AboutStyles";
+
+export default function About() {
+    return (
+        <ContainerView>
+            <Text>Sobre os desenvolvedores</Text>
+        </ContainerView>
+    );
+}
