c3e2be710a4a4784ca3be9fde9b7cc4830c30d7b
hermany13 <hermanyvieiratj@gmail.com>
2020-04-12 23:55:24 -0300

    Global data.

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -27,6 +27,9 @@ export default function Home({ navigation }) {
             <Header open={open} />
             <ContainerView>
                 <Text>Casos: {data.cases}</Text>
+                <Text>Casos de hoje: {data.todayCases}</Text>
+                <Text>Mortes: {data.deaths}</Text>
+                <Text>Mortes de hoje: {data.todayDeaths}</Text>
             </ContainerView>
         </>
     );
