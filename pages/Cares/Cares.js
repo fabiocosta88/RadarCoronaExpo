9dd002df82e9dbd90fa004a01d78ac9ea8672eb5
fabiocosta88 <societyexplode@gmail.com>
2020-04-12 21:27:05 -0300

    New imagens

--- pages/Cares/Cares.js
+++ pages/Cares/Cares.js
@@ -1,7 +1,8 @@
 import React from "react";
+import {View, Text, Image} from 'react-native';
 
 // Styled Components
-import { ContainerView, Text } from "./CaresStyles";
+import styles from "./CaresStyles";
 
 import Header from "../Components/Header/Header";
 
@@ -12,9 +13,31 @@ export default function Cares({ navigation }) {
     return (
         <>
             <Header open={open} />
-            <ContainerView>
-                <Text>Cares</Text>
-            </ContainerView>
+            <View>
+                <Text>
+                    O que você precisa saber e fazer:
+                </Text>
+                <View >
+                    <Image></Image>
+                    <Text>Lave as mãos com água e sabão ou use álcool em gel.</Text>
+                </View>
+                <View >
+                    <Image></Image>
+                    <Text>Cubra o nariz e boca ao espirrar ou tossir.</Text>
+                </View>
+                <View >
+                    <Image></Image>
+                    <Text>Evite aglomerações se estiver doente.</Text>
+                </View>
+                <View >
+                    <Image></Image>
+                    <Text>Mantenha os ambientes bem ventilados.</Text>
+                </View>
+                <View >
+                    <Image></Image>
+                    <Text>Não compartilhe objetos pessoais.</Text>
+                </View>
+            </View>
         </>
     );
 }
